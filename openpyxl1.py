5cf426676c3c0805c2f5dd2cecbdcc5dda5386e5
Raymond <ajayitomisin1728@gmail.com>
2019-12-08 16:50:45 +0100

    My Spreadsheet Project

---
+++ openpyxl1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  8 16:35:41 2019
+
+@author: Ajayi Raymond T
+"""
+
+from openpyxl import Workbook
+workbook = Workbook()
+sheet = workbook.active
+sheet['A1'] = 'hello'
+sheet['B1'] = 'world'
+workbook.save(filename ='my first openpyxl')
